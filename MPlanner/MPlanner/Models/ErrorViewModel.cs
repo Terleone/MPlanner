724be491b884bfcf0ed15d9b8419c71068b5b5aa
eldritch-rudder <eldritch.rudder@gmail.com>
2018-09-15 20:55:59 +0200

    Simple login functionality

---
+++ MPlanner/MPlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
