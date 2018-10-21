6e58768b8ba8629702311a3a7bd204d8727fb3bb
eldritch-rudder <eldritch.rudder@gmail.com>
2018-10-21 15:30:18 +0200

    Cookie consent pop-up removed

--- MPlanner/MPlanner/Startup.cs
+++ MPlanner/MPlanner/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using MPlanner.Services;
-using MPlanner.Services;
 
 namespace MPlanner
 {
@@ -33,7 +32,7 @@ namespace MPlanner
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
